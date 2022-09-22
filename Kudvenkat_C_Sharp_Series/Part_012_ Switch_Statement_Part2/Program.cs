ce0a80dfecaf0ca4e436b6fb8f24932b387561a1
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 17:26:34 +0600

    Part 012 - from label to do while

--- Kudvenkat_C_Sharp_Series/Part_012_ Switch_Statement_Part2/Program.cs
+++ Kudvenkat_C_Sharp_Series/Part_012_ Switch_Statement_Part2/Program.cs
@@ -1,46 +1,49 @@
 ﻿int totalCoffeeCost = 0;
+string? userDecision;
 
-Start:
-int userChoice = 0;
-bool isUserChoiceInt;
 do
 {
-    Console.WriteLine("Select your coffee size: 1 for Small, 2 for Medium, 3 for Large. Write an integer form 1 to 3");
-    isUserChoiceInt = int.TryParse(Console.ReadLine(), out userChoice);
-}while (!isUserChoiceInt);
+    int userChoice = 0;
+    bool isUserChoiceInt;
+    do 
+    {
+        do
+        {
+            Console.WriteLine("Select your coffee size: 1 for Small, 2 for Medium, 3 for Large. Write an integer form 1 to 3");
+            isUserChoiceInt = int.TryParse(Console.ReadLine(), out userChoice);
+        } while (!isUserChoiceInt);
 
 
-switch (userChoice)
-{ 
-    case 1:
-        totalCoffeeCost = totalCoffeeCost + 1;
-        break;
-    case 2:  
-        totalCoffeeCost = totalCoffeeCost + 2;
-        break;
-    case 3:
-        totalCoffeeCost = totalCoffeeCost + 3;
-        break;
-    default:
-        Console.WriteLine("Your Choice {0} is invalid", userChoice);
-        goto Start;
-}
+        switch (userChoice)
+        {
+            case 1:
+                totalCoffeeCost = totalCoffeeCost + 1;
+                break;
+            case 2:
+                totalCoffeeCost = totalCoffeeCost + 2;
+                break;
+            case 3:
+                totalCoffeeCost = totalCoffeeCost + 3;
+                break;
+            default:
+                Console.WriteLine("Your Choice {0} is invalid", userChoice);
+                //goto Start
+                break;
+        }
+    } while (userChoice !=1 && userChoice !=2 && userChoice !=3);
 
-Decide:
-Console.WriteLine("Do you want to buy another coffee - Yes or No?");
-string? userDecision = Console.ReadLine();
+    
+    do
+    {
+        Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+        userDecision = Console.ReadLine().ToUpper();
 
-switch (userDecision.ToUpper())
-{
-    case "YES":
-        goto Start;
-    case "NO":
-        break;
-    default:
-        Console.WriteLine("Your choice {0} is invalid. Please try again", userDecision);
-        goto Decide;
-
-}
+        if (userDecision != "YES" && userDecision != "NO")
+        {
+                Console.WriteLine("Your choice {0} is invalid. Please try again", userDecision);
+        }
+    } while (userDecision!="YES" && userDecision != "NO");
+} while (userDecision.ToUpper() != "NO");
 
 Console.WriteLine("Thank you for shopping with us");
 Console.WriteLine("Bill amount = {0} ", totalCoffeeCost);
