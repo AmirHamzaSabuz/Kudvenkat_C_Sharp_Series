71a22cdc8ada16ddcccf55c630fbb5c984dc9420
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 12:01:32 +0600

    Part 002 Concatenation

--- Kudvenkat_C_Sharp_Series/Part_002_Reading_And_Writing/Program.cs
+++ Kudvenkat_C_Sharp_Series/Part_002_Reading_And_Writing/Program.cs
@@ -8,7 +8,8 @@
         Console.WriteLine("Please enter your last name");
         string? LastName = Console.ReadLine();
 
-        Console.WriteLine("Hello {0} {1}",  FirstName, LastName);
+        Console.WriteLine("Hello {0} {1}",  FirstName, LastName); // Placeholder Syntax
+        Console.WriteLine("Your full name is " + FirstName + " " + LastName); // Concatenation
 
         Console.ReadKey();
 
